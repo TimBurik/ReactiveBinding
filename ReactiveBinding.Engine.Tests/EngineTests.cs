66460984cfe08e3844ec29e17af5ecefe97a05bf
Timofey Burak <timofey.burak@softeq.com>
2022-11-20 13:32:07 +0100

    Refactor test names

--- ReactiveBinding.Engine.Tests/EngineTests.cs
+++ ReactiveBinding.Engine.Tests/EngineTests.cs
@@ -6,7 +6,7 @@ namespace ReactiveBinding.Engine.Tests;
 public class EngineTests
 {
     [Fact]
-    public void Target_receive_values_from_source()
+    public void Binding_passes_values_from_source_to_target()
     {
         var source = new Subject<string>();
         var target = Substitute.For<IObserver<string>>();
@@ -18,7 +18,7 @@ public class EngineTests
     }
 
     [Fact]
-    public void Binding_stop_working_when_disposed()
+    public void Disposed_binding_does_not_pass_values_from_source_to_target()
     {
         var source = new Subject<string>();
         var target = Substitute.For<IObserver<string>>();
@@ -31,7 +31,7 @@ public class EngineTests
     }
 
     [Fact]
-    public void Source_receives_updates_from_target()
+    public void Binding_passes_values_from_target_to_source()
     {
         var source = Substitute.For<IObserver<string>>();
         var target = new Subject<string>();
@@ -41,9 +41,22 @@ public class EngineTests
         
         source.Received().OnNext("test");
     }
+    
+    [Fact]
+    public void Disposed_binding_does_not_pass_values_from_target_to_source()
+    {
+        var source = Substitute.For<IObserver<string>>();
+        var target = new Subject<string>();
+
+        var binding = new Binding(source, target);
+        binding.Dispose();
+        target.OnNext("test");
+        
+        source.DidNotReceive().OnNext(Arg.Any<string>());
+    }
 
     [Fact]
-    public void Binging_is_bidirectional()
+    public void Bidirectional_binding_passes_values_from_source_to_target_and_from_target_to_source()
     {
         var source = new Subject<string>();
         var sourceListener = Substitute.For<IObserver<string>>();
@@ -59,5 +72,57 @@ public class EngineTests
 
         sourceListener.Received().OnNext("target");
         targetListener.Received().OnNext("source");
+    }
+
+    [Fact]
+    public void Disposed_bidirectional_binding_does_not_pass_values_from_source_to_target()
+    {
+        var source = new Subject<string>();
+
+        var target = new Subject<string>();
+        var targetListener = Substitute.For<IObserver<string>>();
+        target.Subscribe(targetListener);
+        
+        var binding = new Binding(source, target);
+        binding.Dispose();
+        source.OnNext("source");
+        
+        targetListener.DidNotReceive().OnNext(Arg.Any<string>());
+    }
+    
+    [Fact]
+    public void Disposed_bidirectional_binding_does_not_pass_values_from_target_to_source()
+    {
+        var source = new Subject<string>();
+        var sourceListener = Substitute.For<IObserver<string>>();
+        source.Subscribe(sourceListener);
+
+        var target = new Subject<string>();
+
+        var binding = new Binding(source, target);
+        binding.Dispose();
+        target.OnNext("target");
+        
+        sourceListener.DidNotReceive().OnNext(Arg.Any<string>());
+    }
+    
+    [Fact]
+    public void Disposed_bidirectional_binding_does_not_pass_values_both_ways()
+    {
+        var source = new Subject<string>();
+        var sourceListener = Substitute.For<IObserver<string>>();
+        source.Subscribe(sourceListener);
+        
+        var target = new Subject<string>();
+        var targetListener = Substitute.For<IObserver<string>>();
+        target.Subscribe(targetListener);
+        
+        var binding = new Binding(source, target);
+        binding.Dispose();
+        source.OnNext("source");
+        target.OnNext("target");
+
+        sourceListener.DidNotReceive().OnNext("target");
+        targetListener.DidNotReceive().OnNext("source");
     }
 }
